ceba2cf9d1d0c7b39191b6210adbfdb83bffaee8
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-11-17 19:00:21 +0000

    Add semi-colon

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var fs = require('fs');
 module.exports = {
   getdata: function getdata(dir) {
     // body...
-    return YAML.parse(fs.readFileSync(dir,'utf8'))
+    return YAML.parse(fs.readFileSync(dir,'utf8'));
   }
 }
